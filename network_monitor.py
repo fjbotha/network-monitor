cf2f90c5509ad55a8e5de7158772b5b2479e6619
Frik Botha <fjbotha@hotmail.com>
2022-04-22 07:42:20 +0200

    Add desktop notifications
    This is not rounded off very nicely, but it works.
    
    TODO: Investigate basing the notifications on the Notify package as in
    the commented code. Check what the bash script does to get it working.
    
    TODO: Add logging.

--- network_monitor.py
+++ network_monitor.py
@@ -3,6 +3,16 @@ import argparse
 from scapy.all import *
 import time
 import sys
+import subprocess
+import datetime
+#from gi.repository import Notify
+
+
+def _usage():
+    parser = argparse.ArgumentParser(description="ping-based network monitor")
+    parser.add_argument("-d", "--dst-ip", help="Destination IP",
+                        type=str, default="8.8.8.8")
+    return parser
 
 
 def ping1(ip):
@@ -22,23 +32,33 @@ def ping1(ip):
 
 def beep():
     for i in range(0, 10):
-        print('\a', flush=True)
+        print('\r\a', end='', flush=True)
         time.sleep(50e-3)
-
-
-def _usage():
-    parser = argparse.ArgumentParser(description="ping-based network monitor")
-    parser.add_argument("-d", "--dst-ip", help="Destination IP",
-                        type=str, default="8.8.8.8")
-    return parser
 
 
 def main():
     args = _usage().parse_args()
+    #Notify.init('Network Monitor')
+
+    uptime = datetime.datetime.now()
+    last_notify = datetime.datetime(1970, 1, 1)
     while True:
         s = ping1(args.dst_ip)
         if s is None:
             beep()
+            now = datetime.datetime.now()
+            offline_duration = now - uptime
+            delta = offline_duration.total_seconds()
+            if delta > 1:
+                if (now - last_notify).total_seconds() > 30:
+                    msg = "Network monitor: Offline for %.1f seconds." % delta
+                    print(msg)
+                    # notifier = Notify.Notification.new(msg)
+                    # notifier.show()
+                    subprocess.run(["./notify_send.sh", msg])
+                    last_notify = datetime.datetime.now()
+        else:
+            uptime = datetime.datetime.now()
         time.sleep(1)
 
 
